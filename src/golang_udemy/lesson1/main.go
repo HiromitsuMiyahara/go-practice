0a412d0a1e2f00a636c5cd471b3f2412375ba8b0
miyahara <0912.hiromi.0912@gmail.com>
2026-02-08 18:50:10 +0900

    ポインタ

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -4,10 +4,36 @@ import (
 	"fmt"
 )
 
-func main() {
-	ch := make(chan int)
-	fmt.Printf("%T\n", ch)
+type Vertex struct {
+	X, Y int
+	S    string
+}
 
-	var st = new(struct{})
-	fmt.Printf("%T\n", st)
+func chageVertex(v Vertex) {
+	v.X = 1000
+}
+
+func main() {
+	/*
+	v2 := Vertex{X: 1}
+	fmt.Println(v2)
+	fmt.Println(v2.X, v2.Y)
+	v2.X = 100
+	fmt.Println(v2.X, v2.Y)
+
+	v3 := Vertex{1, 2, "test"}
+	fmt.Println(v3)
+
+	v4 := Vertex{}
+	fmt.Println(v4)
+
+	var v5 Vertex
+	fmt.Println(v5)
+	*/
+
+	v6 := new(Vertex)
+	fmt.Printf("%T %v\n", v6, v6)
+
+	v7 := &Vertex{}
+	fmt.Printf("%T %v\n", v7, v7)
 }
