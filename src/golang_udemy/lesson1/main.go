814da1d4482db2439cc8eb438807fa57bb78cf39
miyahara <0912.hiromi.0912@gmail.com>
2026-02-08 19:10:23 +0900

    ポインタ

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -9,31 +9,12 @@ type Vertex struct {
 	S    string
 }
 
-func chageVertex(v Vertex) {
-	v.X = 1000
+func chageVertex2(v *Vertex) {
+	(*v).X = 1000
 }
 
 func main() {
-	/*
-	v2 := Vertex{X: 1}
-	fmt.Println(v2)
-	fmt.Println(v2.X, v2.Y)
-	v2.X = 100
-	fmt.Println(v2.X, v2.Y)
-
-	v3 := Vertex{1, 2, "test"}
-	fmt.Println(v3)
-
-	v4 := Vertex{}
-	fmt.Println(v4)
-
-	var v5 Vertex
-	fmt.Println(v5)
-	*/
-
-	v6 := new(Vertex)
-	fmt.Printf("%T %v\n", v6, v6)
-
-	v7 := &Vertex{}
-	fmt.Printf("%T %v\n", v7, v7)
+	v := &Vertex{1, 2, "test"}
+	chageVertex2(v)
+	fmt.Println(v)
 }
