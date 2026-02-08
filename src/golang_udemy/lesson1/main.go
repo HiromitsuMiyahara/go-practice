e59a645ccc5fb80188b8f2f5f45d06c5e90a94cb
miyahara <0912.hiromi.0912@gmail.com>
2026-02-08 20:21:23 +0900

    メソッド

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -6,15 +6,19 @@ import (
 
 type Vertex struct {
 	X, Y int
-	S    string
 }
 
-func chageVertex2(v *Vertex) {
-	(*v).X = 1000
+func (v Vertex) Area() int {
+	return v.X * v.Y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
 }
 
 func main() {
-	v := &Vertex{1, 2, "test"}
-	chageVertex2(v)
-	fmt.Println(v)
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Area())
 }
