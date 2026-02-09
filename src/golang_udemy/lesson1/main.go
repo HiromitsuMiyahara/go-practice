60f463dd1ba9544e043ab1b9d39dc7d6279db1fa
miyahara <0912.hiromi.0912@gmail.com>
2026-02-09 23:20:59 +0900

    ダックタイピング

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -5,19 +5,54 @@ import (
 )
 
 type Human interface {
-	Say()
+	Say() string
+}
+
+type Pet interface {
+	One() string
 }
 
 type Person struct {
 	Name string
 }
 
-func (p *Person) Say() {
+func (p *Person) Say() string {
 	p.Name = "Mr." + p.Name
 	fmt.Println(p.Name)
+	return p.Name
+}
+
+type Dog struct {
+	Name string
+}
+
+func (d *Dog) One() string {
+	d.Name = "Big" + d.Name
+	fmt.Println(d.Name)
+	return d.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("Run")
+	} else {
+		fmt.Println("Get out")
+	}
+}
+
+func DriveCar2(pet Pet) {
+	if pet.One() == "Bigdog" {
+		fmt.Println("Run")
+	} else {
+		fmt.Println("Get out")
+	}
 }
 
 func main() {
 	var mike Human = &Person{"Mike"}
-	mike.Say()
+	var x Human = &Person{"X"}
+	var dog Pet = &Dog{"dog"}
+	DriveCar(mike)
+	DriveCar(x)
+	DriveCar2(dog)
 }
