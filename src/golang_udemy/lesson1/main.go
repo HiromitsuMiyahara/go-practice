0144ba4f54155bed799193beb1bbf831d2a305cf
miyahara <0912.hiromi.0912@gmail.com>
2026-02-09 22:11:41 +0900

    埋め込み

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -5,20 +5,41 @@ import (
 )
 
 type Vertex struct {
-	X, Y int
+	x,y int
 }
 
 func (v Vertex) Area() int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
 }
 
+type Vertex3D struct {
+	Vertex
+	z int
+}
+
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
+}
+
+
 func main() {
-	v := Vertex{3, 4}
+	v := New(3, 4, 5)
 	v.Scale(10)
-	fmt.Println(v.Area())
+	fmt.Println(v.Area()) // 1200
+	fmt.Println(v.Area3D()) // 6000
 }
