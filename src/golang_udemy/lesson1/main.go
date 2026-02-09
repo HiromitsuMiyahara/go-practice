9c475c833afee1c0a0dc6e411a0ae23c4453415f
miyahara <0912.hiromi.0912@gmail.com>
2026-02-09 22:37:13 +0900

    インタフェース

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -4,42 +4,20 @@ import (
 	"fmt"
 )
 
-type Vertex struct {
-	x,y int
+type Human interface {
+	Say()
 }
 
-func (v Vertex) Area() int {
-	return v.x * v.y
+type Person struct {
+	Name string
 }
 
-func (v *Vertex) Scale(i int) {
-	v.x = v.x * i
-	v.y = v.y * i
+func (p *Person) Say() {
+	p.Name = "Mr." + p.Name
+	fmt.Println(p.Name)
 }
-
-type Vertex3D struct {
-	Vertex
-	z int
-}
-
-func (v Vertex3D) Area3D() int {
-	return v.x * v.y * v.z
-}
-
-func (v *Vertex3D) Scale3D(i int) {
-	v.x = v.x * i
-	v.y = v.y * i
-	v.z = v.z * i
-}
-
-func New(x, y, z int) *Vertex3D {
-	return &Vertex3D{Vertex{x, y}, z}
-}
-
 
 func main() {
-	v := New(3, 4, 5)
-	v.Scale(10)
-	fmt.Println(v.Area()) // 1200
-	fmt.Println(v.Area3D()) // 6000
+	var mike Human = &Person{"Mike"}
+	mike.Say()
 }
