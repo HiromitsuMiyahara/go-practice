8779c99b2c049a796d6252dc0695db539e60babd
miyahara <0912.hiromi.0912@gmail.com>
2026-02-10 13:13:56 +0900

    型アサーションint

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -4,55 +4,13 @@ import (
 	"fmt"
 )
 
-type Human interface {
-	Say() string
-}
-
-type Pet interface {
-	One() string
-}
-
-type Person struct {
-	Name string
-}
-
-func (p *Person) Say() string {
-	p.Name = "Mr." + p.Name
-	fmt.Println(p.Name)
-	return p.Name
-}
-
-type Dog struct {
-	Name string
-}
-
-func (d *Dog) One() string {
-	d.Name = "Big" + d.Name
-	fmt.Println(d.Name)
-	return d.Name
-}
-
-func DriveCar(human Human) {
-	if human.Say() == "Mr.Mike" {
-		fmt.Println("Run")
-	} else {
-		fmt.Println("Get out")
-	}
-}
-
-func DriveCar2(pet Pet) {
-	if pet.One() == "Bigdog" {
-		fmt.Println("Run")
-	} else {
-		fmt.Println("Get out")
-	}
+func do (i interface{}) {
+	ii := i.(int)
+	ii *= 2
+	fmt.Println(ii)
 }
 
 func main() {
-	var mike Human = &Person{"Mike"}
-	var x Human = &Person{"X"}
-	var dog Pet = &Dog{"dog"}
-	DriveCar(mike)
-	DriveCar(x)
-	DriveCar2(dog)
+	var i interface{} = 10
+	do(i)
 }
