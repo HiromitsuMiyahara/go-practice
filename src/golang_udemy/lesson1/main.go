d70306a7a7681fd756690937dab139f43f9ed4fc
miyahara <0912.hiromi.0912@gmail.com>
2026-02-10 13:25:50 +0900

    switch

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -5,12 +5,18 @@ import (
 )
 
 func do (i interface{}) {
-	ii := i.(int)
-	ii *= 2
-	fmt.Println(ii)
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	default:
+		fmt.Printf("I don`t know %T\n", v)
+	}
 }
 
 func main() {
-	var i interface{} = 10
-	do(i)
+	do(10)
+	do("Mike")
+	do(true)
 }
