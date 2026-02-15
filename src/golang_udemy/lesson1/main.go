6be9746664b50b6d0d1a03fad51ce26e672e1bf3
miyahara <0912.hiromi.0912@gmail.com>
2026-02-15 22:05:25 +0900

    select

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -20,6 +20,14 @@ func goroutine2(s int[], c chan int) {
 	c <- sum
 }
 
+func goroutine3(s int[], c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
 func main() {
 	s := int[1, 2, 3, 4, 5]
 	c = make(chan int)
