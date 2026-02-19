cd88c40a26e3b060dcd2a74bb06254f7db7b6509
miyahara <0912.hiromi.0912@gmail.com>
2026-02-19 23:36:58 +0900

    select

--- src/golang_udemy/lesson1/main.go
+++ src/golang_udemy/lesson1/main.go
@@ -2,43 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
-
-func goroutine1(ch chan string) {
-	for {
-		ch <- "packet from 1"
-		time.Sleep(1 * time.Second)
-		}
-}
-
-func goroutine2(ch chan string) {
-	for {
-		ch <- "packet from 2"
-		time.Sleep(1 * time.Second)
-		}
-}
-
-func goroutine3(ch chan string) {
-	for {
-		ch <- "packet from 2"
-		time.Sleep(1 * time.Second)
-		}
-}
-
-func goroutine4(ch chan string) {
-	for {
-		ch <- "packet from 2"
-		time.Sleep(1 * time.Second)
-		}
-}
-
-func goroutine5(ch chan string) {
-	for {
-		ch <- "packet from 2"
-		time.Sleep(1 * time.Second)
-		}
-}
 
 func main() {
 	c1 := make(chan string)
